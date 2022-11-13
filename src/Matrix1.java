59a3c1e4ae8edeb85a94ea4b1881b82bcff310f2
Serhii <Sergukxxx1990@gmail.com>
2022-11-13 13:36:25 +0200

    Задание с массивом

---
+++ src/Matrix1.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Matrix1 {
+    public static void main(String[] args) {
+        System.out.println("Введите размер массива");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        int[][] matrix = new int[x][y];
+        int [][] matrixA=new int [x][y];
+
+        for (int i = 0; i < x; i++) {
+
+            for (int j = 0; j < y; j++) {
+                System.out.print("Введите элемент matrix : [" + i + "][" + j + "]:");
+                int sc1 = scanner.nextInt();
+                matrix[i][j] = sc1;
+                matrixA[i][j]=sc1;
+
+            }
+            System.out.println("" + Arrays.deepToString(matrix));
+
+        }
+
+        //
+        scanner.close();
+
+        System.out.println("-----------------------");
+        for (int i = 0; i < x; i++) {
+
+            for (int j = 0; j < y; j++) {
+
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+
+        }
+
+
+        System.out.println("---------------------");
+
+        for(int i=matrixA.length-1;i>=0;i--){
+            for (int j=matrixA[i].length-1;j>=0;j--){
+                System.out.print(matrixA[i][j]+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
